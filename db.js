bf83e325383c0d49caf48da65785db132ff2f41c
Jahid-CodeClouds <jahid.moula@codeclouds.com>
2024-01-17 17:05:30 +0530

    updated: added mongodb implementations

--- db.js
+++ db.js
@@ -7,19 +7,47 @@ class Connect {
     this.config = config;
   }
 
-  mongoCRUD = async (type, resource, filters = {}) => {
+  mongoCRUD = async (
+    type,
+    resource,
+    filters = {},
+    payload = {},
+    options = {}
+  ) => {
     let mongooseConnection = await mongoose
       .connect(this.config)
       .catch((err) => console.log("Connection error: " + err));
 
     const cruder = new CRUD(mongooseConnection.connection);
+    
     let response = null;
     switch (type) {
       case "list":
         response = await cruder.list(resource, filters);
         break;
+      case "find":
+        response = await cruder.find(resource, filters);
+        break;
+      case "createOne":
+        response = await cruder.createOne(resource, payload);
+        break;
+      case "createMany":
+        response = await cruder.createMany(resource, payload);
+        break;
+      case "updateOne":
+        response = await cruder.updateOne(resource, payload, filters, options);
+        break;
+      case "updateMany":
+        response = await cruder.updateMany(resource, payload, filters, options);
+        break;
+      case "removeOne":
+        response = await cruder.removeOne(resource, filters);
+        break;
+      case "removeMany":
+        response = await cruder.removeMany(resource, filters);
+        break;
       default:
-        response = "Invalid Payload";
+        response = "Invalid type";
     }
 
     return response;
